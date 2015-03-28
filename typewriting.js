e7e65c4cb734561f2adb7ee4f5748b8ca0408a8b
EddieWen-Taiwan <m20021991y@gmail.com>
2015-03-28 17:09:03 +0800

    Add a new function - rewrite

--- typewriting.js
+++ typewriting.js
@@ -6,6 +6,12 @@
 
 	var part1 = '@-webkit-keyframes blink { 0% { opacity: 1; } 50% { opacity: 0; } 100% { opacity: 1; } } @-moz-keyframes blink { 0% { opacity: 1; } 50% { opacity: 0; } 100% { opacity: 1; } } @keyframes blink { 0% { opacity: 1; } 50% { opacity: 0; } 100% { opacity: 1; } } ';
 
+	var target;
+	var currentNumber;
+	var inputString = "";
+	var setting;
+	var callback;
+
 	var inHtmlTag;
 
 	$.fn.typewriting = function( input_string, options, callback_func ) {
@@ -14,58 +20,62 @@
 		var cursorHeight = this.height();
 		this.text("");
 
-		var currentNumber = 0;
-		var setting = $.extend({
+		currentNumber = 0;
+		inputString = input_string;
+		setting = $.extend({
 			typing_interval: 150,
 			blink_interval: "0.7s",
 			cursor_color: "black"
 		}, options);
+		callback = callback_func;
 
 		var part2 = '.typingCursor::after { content: ""; width: 10px; height: ' + cursorHeight + 'px; margin-left: 5px; display: inline-block; vertical-align: bottom; background-color: ' + setting.cursor_color + '; -webkit-animation: blink ' + setting.blink_interval + ' infinite; -moz-animation: blink ' + setting.blink_interval + ' infinite; animation: blink ' + setting.blink_interval + ' infinite; }';
 
 		$('head').append( '<style type="text/css">' + part1 + part2 + '</style>' );
 
-		var target = this.addClass("typingCursor");
+		target = this.addClass("typingCursor");
 
 		typingGo();
 
-
-
-		function typingGo() {
-
-			if( currentNumber <= input_string.length ) {
-
-				var thisText = getText();
-
-				if( thisText.slice(-1) == "<" ) inHtmlTag = true;
-				if( thisText.slice(-1) == ">" ) inHtmlTag = false;
-
-				target.html( thisText );
-
-				if( inHtmlTag )
-					typingGo();
-				else
-					setTimeout( function(){
-						typingGo();
-					}, setting.typing_interval);
-
-			} else {
-				if( callback_func ) callback_func();
-			}
-		}
-
-
-
-		function getText() {
-			var returnString = input_string.slice( 0, currentNumber );
-			currentNumber++;
-			return returnString;
-		}
-
 	};
 
-	$.fn.typeagain = function() {
-		
+	$.fn.rewrite = function( input_string, callback_func ) {
+
+		currentNumber = 0;
+		inputString = input_string;
+		callback = callback_func;
+
+		typingGo();
+
+	}
+
+	function typingGo() {
+
+		if( currentNumber <= inputString.length ) {
+
+			var thisText = getText();
+
+			if( thisText.slice(-1) == "<" ) inHtmlTag = true;
+			if( thisText.slice(-1) == ">" ) inHtmlTag = false;
+
+			target.html( thisText );
+
+			if( inHtmlTag )
+				typingGo();
+			else
+				setTimeout( function(){
+					typingGo();
+				}, setting.typing_interval);
+
+		} else {
+			if( callback ) callback();
+		}
+	}
+
+	function getText() {
+		var returnString = inputString.slice( 0, currentNumber );
+		currentNumber++;
+		return returnString;
 	}
 
 }(jQuery));
