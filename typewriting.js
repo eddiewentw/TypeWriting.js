7c9821367b14422b4c507cd49261a734baf6d459
EddieWen-Taiwan <m20021991y@gmail.com>
2016-02-21 19:58:16 +0800

    Use single-quotes  instead of double-quotes

--- typewriting.js
+++ typewriting.js
@@ -13,24 +13,24 @@
 	$.fn.typewriting = function( input_string, options, callback_func ) {
 
 		// Get the height of cursor should be
-		this.text("A");
+		this.text('A');
 		var cursorHeight = this.height();
-		this.text("");
+		this.text('');
 
 		// Store setting and function from user
 		setting = $.extend({
 			typing_interval	: 150,
-			blink_interval	: "0.7s",
-			cursor_color	: "black",
-			inputString 	: "",
+			blink_interval	: '0.7s',
+			cursor_color	: 'black',
+			inputString 	: '',
 		}, options);
 		setting.inputString = input_string;
 		callback = callback_func;
 
 		// Add cursor style in HEAD
-		$('head').append( `<style type="text/css">@-webkit-keyframes blink{0%,100%{opacity:1}50%{opacity:0}}@-moz-keyframes blink{0%,100%{opacity:1}50%{opacity:0}}@keyframes blink{0%,100%{opacity:1}50%{opacity:0}}.typingCursor::after{content:"";width:10px;height:${cursorHeight}px;margin-left:5px;display:inline-block;vertical-align:bottom;background-color:${setting.cursor_color};-webkit-animation:blink ${setting.blink_interval} infinite;-moz-animation:blink ${setting.blink_interval} infinite;animation:blink ${setting.blink_interval} infinite}</style>` );
+		$('head').append( `<style type='text/css'>@-webkit-keyframes blink{0%,100%{opacity:1}50%{opacity:0}}@-moz-keyframes blink{0%,100%{opacity:1}50%{opacity:0}}@keyframes blink{0%,100%{opacity:1}50%{opacity:0}}.typingCursor::after{content:'';width:10px;height:${cursorHeight}px;margin-left:5px;display:inline-block;vertical-align:bottom;background-color:${setting.cursor_color};-webkit-animation:blink ${setting.blink_interval} infinite;-moz-animation:blink ${setting.blink_interval} infinite;animation:blink ${setting.blink_interval} infinite}</style>` );
 
-		_typingGo( this.addClass("typingCursor") );
+		_typingGo( this.addClass('typingCursor') );
 
 	};
 
@@ -51,10 +51,10 @@
 
 			var thisText = _getText();
 
-			if( thisText.slice(-1) == "<" ) {
+			if( thisText.slice(-1) == '<' ) {
 				inHtmlTag = true;
 			}
-			else if( thisText.slice(-1) == ">" ) {
+			else if( thisText.slice(-1) == '>' ) {
 				inHtmlTag = false;
 			}
 
