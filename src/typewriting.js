2bebf7d448e4fa400cad891c372d96c9766d96cc
EddieWen-Taiwan <m20021991y@gmail.com>
2016-03-10 11:11:16 +0800

    Delete jquery version

--- src/typewriting.js
+++ src/typewriting.js
@@ -1,134 +1,156 @@
-(function($){
+(function() {
+	
+	this.TypeWriting = function( options, callback_func ) {
 
-	/* ---------------------------------- *\
-					打 字 機
-	\* ---------------------------------- */
+		// global variables
+		this._currentNumber = 0;
+		this._inHTMLTag = false;
 
-	var settings;
-
-	var _currentNumber = 0;
-	var _inHTMLTag = false;
-
-	$.fn.typewriting = function( input_string, options, callback_func ) {
-
-		// Get the height of cursor should be
-		const cursorHeight = this.height();
-		const cursorWidth = parseInt(cursorHeight/3);
-
-		// Store setting and function from user
-		settings = $.extend({
+		var settings = {
+			targetElement	: null,
+			inputString 	: '',
 			typing_interval	: 150,
 			blink_interval	: '0.7s',
 			cursor_color	: 'black',
-			inputString 	: '',
 			tw_callback		: function(){},
 			task			: 'unready',
-		}, options);
+		}
+		if( options && typeof options === "object" ) {
+			this.settings = extendDefaults(settings, options);
+		}
 
-		// Handle inputString ---required
-		if( input_string ) {
-			if( typeof input_string == 'string' )
-				settings.inputString = input_string;
-			else
-				throw new Error(`${input_string} is not a string`);
+		// check inputString ---required
+		if( options.inputString ) {
+			if( typeof options.inputString !== 'string' )
+				throw new Error(`${options.inputString} is not a string`);
 		}
 		else
-			throw new Error('Missing argument: String');
+			throw new Error('Missing argument: inputString');
 
-		// Handle callback
+		// check callback
 		if( callback_func ) {
-			if( typeof callback_func == 'function' )
-				settings.tw_callback = callback_func;
+			if( typeof callback_func === 'function' )
+				this.settings.tw_callback = callback_func;
 			else {
 				console.error(`${callback_func} is not a function`);
-				_cleanCallback();
+				_cleanCallback.call(this);
 			}
 		} else
-			_cleanCallback();
+			_cleanCallback.call(this);
 
-		// Add cursor style in HEAD
-		$('head').append( `<style type='text/css'>@-webkit-keyframes blink{0%,100%{opacity:1}50%{opacity:0}}@-moz-keyframes blink{0%,100%{opacity:1}50%{opacity:0}}@keyframes blink{0%,100%{opacity:1}50%{opacity:0}}.typingCursor::after{content:'';width:${cursorWidth}px;height:${cursorHeight}px;margin-left:5px;display:inline-block;vertical-align:bottom;background-color:${settings.cursor_color};-webkit-animation:blink ${settings.blink_interval} infinite;-moz-animation:blink ${settings.blink_interval} infinite;animation:blink ${settings.blink_interval} infinite}</style>` );
+		// get the height of cursor should be
+		var cursorHeight = this.settings.targetElement.offsetHeight;
+		var cursorWidth = parseInt(cursorHeight/3);
+		if( cursorHeight == 0 ) {
+			this.settings.targetElement.innerHTML = 'I';
+			cursorHeight = this.settings.targetElement.offsetHeight;
+			cursorWidth = this.settings.targetElement.offsetWidth;
+		}
 
-		settings.task = 'typing';
-		_typingGo( this.addClass('typingCursor') );
+		// prepare cursor style
+		const head = document.head;
+		const cssStyle = `@-webkit-keyframes blink{0%,100%{opacity:1}50%{opacity:0}}@-moz-keyframes blink{0%,100%{opacity:1}50%{opacity:0}}@keyframes blink{0%,100%{opacity:1}50%{opacity:0}}.typingCursor::after{content:'';width:${cursorWidth}px;height:${cursorHeight}px;margin-left:5px;display:inline-block;vertical-align:bottom;background-color:${settings.cursor_color};-webkit-animation:blink ${settings.blink_interval} infinite;-moz-animation:blink ${settings.blink_interval} infinite;animation:blink ${settings.blink_interval} infinite}`;
+		var styleNode = document.createElement('style');
+			styleNode.type = 'text/css';
+		if( styleNode.styleSheet )
+			styleNode.styleSheet.cssText = cssStyle;
+		else
+			styleNode.appendChild(document.createTextNode(cssStyle));
+		// add CSS style in HEAD
+		head.appendChild(styleNode);
 
-	};
+		this.settings.targetElement.className += 'typingCursor';
+		this.settings.task = 'typing';
+		_typingGo.call(this);
 
-	$.fn.rewrite = function( input_string, callback_func ) {
+	}
 
-		if( settings.task == 'typing' ) {
+	TypeWriter.prototype.rewrite = function( input_string, callback_func ) {
+
+		if( this.settings.task == 'typing' ) {
 			console.warn( 'Last task is not finished yet.' );
 			setTimeout( function() {
 				this.rewrite( input_string, callback_func );
-			}.bind(this), settings.typing_interval );
+			}.bind(this), this.settings.typing_interval );
 		}
 		else {
 			// Handle inputString ---required
 			if( input_string ) {
 				if( typeof input_string == 'string' )
-					settings.inputString = input_string;
+					this.settings.inputString = input_string;
 				else
 					console.error(`${input_string} is not a string`);
 			}
 			else
-				throw new Error('Missing argument: String');
+				throw new Error('Missing argument: inputString');
 
 			// Handle callback
 			if( callback_func ) {
 				if( typeof callback_func == 'function' )
-					settings.tw_callback = callback_func;
+					this.settings.tw_callback = callback_func;
 				else {
 					throw new Error(`${callback_func} is not a function`);
-					_cleanCallback();
+					_cleanCallback.call(this);
 				}
 			}
 			else
-				_cleanCallback();
+				_cleanCallback.call(this);
 
-			settings.task = 'typing';
-			_typingGo( this );
+			this.settings.task = 'typing';
+			_typingGo.call(this);
 		}
 
 	}
 
-	function _typingGo( target ) {
+	function _typingGo() {
 
-		if( _currentNumber < settings.inputString.length ) {
+		if( this._currentNumber < this.settings.inputString.length ) {
 
-			const thisText = _getText();
+			const thisText = _getText.call(this);
 
 			if( thisText.slice(-1) == '<' ) {
-				_inHTMLTag = true;
+				this._inHTMLTag = true;
 			}
 			else if( thisText.slice(-1) == '>' ) {
-				_inHTMLTag = false;
+				this._inHTMLTag = false;
 			}
 
-			target.html( thisText );
+			this.settings.targetElement.innerHTML = thisText;
 
-			if( _inHTMLTag )
-				_typingGo( target );
+			if( this._inHTMLTag )
+				_typingGo.call(this);
 			else {
 				setTimeout( function() {
-					_typingGo( target );
-				}, settings.typing_interval);
+					_typingGo.call(this);
+				}.bind(this), this.settings.typing_interval);
 			}
 
 		}
 		else {
-			settings.task = 'ready';
-			_currentNumber = 0;
-			settings.tw_callback.call(this);
+			this.settings.task = 'ready';
+			this._currentNumber = 0;
+			this.settings.tw_callback.call();
 		}
+
 	}
 
 	function _getText() {
-		return settings.inputString.slice( 0, ++_currentNumber );
+		return this.settings.inputString.slice( 0, ++this._currentNumber );
 	}
 
 	function _cleanCallback() {
-		settings.tw_callback = function(){};
+		this.settings.tw_callback = function(){};
 	}
 
-}(jQuery));
+	// Utility method to extend defaults with user options
+	function extendDefaults(source, properties) {
+		var property;
+		for( property in properties ) {
+			if( properties.hasOwnProperty(property) ) {
+				source[property] = properties[property];
+			}
+		}
+		return source;
+	}
 
+}());
