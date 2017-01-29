1b9a71ade0850de2010a6d133ea690b748630bca
Eddie Wen <eddiewen.tw@gmail.com>
2017-01-29 20:22:23 +0800

    Install gulp-rename
    rename dist file

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,7 @@
 import gulp from 'gulp';
 import babel from 'gulp-babel';
 import uglify from 'gulp-uglify';
+import rename from 'gulp-rename';
 
 gulp.task('minify', () => 
 	gulp.src('./src/typewriting.js')
@@ -8,7 +9,8 @@ gulp.task('minify', () =>
 		.pipe( uglify({
 			preserveComments: 'license',
 		}) )
-		.pipe( gulp.dest('./build') )
+		.pipe( rename('typewriting.min.js') )
+		.pipe( gulp.dest('./dist') )
 );
 
 gulp.task('default', [ 'minify' ]);
