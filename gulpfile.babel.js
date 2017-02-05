60c4f176e9b38474b5129d51d36040f8d8b43f68
Eddie Wen <eddiewen.tw@gmail.com>
2017-02-05 14:34:15 +0800

    Add sourcemap of .min.js
    Install gulp-sourcemaps to make a sourcemap

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,14 +2,17 @@ import gulp from 'gulp';
 import babel from 'gulp-babel';
 import uglify from 'gulp-uglify';
 import rename from 'gulp-rename';
+import sourcemap from 'gulp-sourcemaps';
 
 gulp.task('minify', () => 
 	gulp.src('./src/typewriting.js')
+		.pipe( sourcemap.init() )
 		.pipe( babel() )
 		.pipe( uglify({
 			preserveComments: 'license',
 		}) )
 		.pipe( rename('typewriting.min.js') )
+		.pipe( sourcemap.write('.') )
 		.pipe( gulp.dest('./dist') )
 );
 
